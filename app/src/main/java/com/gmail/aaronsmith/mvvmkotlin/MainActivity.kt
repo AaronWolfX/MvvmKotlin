e1a9a84e3b3ab0a9c92ca3db6178bf7da7a4c137
Aaron Smith <aaron_wolfx@163.com>
2019-03-28 22:49:41 +0800

    dateBinding 的简单使用

--- app/src/main/java/com/gmail/aaronsmith/mvvmkotlin/MainActivity.kt
+++ app/src/main/java/com/gmail/aaronsmith/mvvmkotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.gmail.aaronsmith.mvvmkotlin
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gmail.aaronsmith.mvvmkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var activityMainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        var user = User()
+        activityMainBinding.userInfo = user
+        user.name = "Aaron"
+        user.password = "123456"
+
+        user.name = "Smith"
     }
 }
---
+++ app/src/main/java/com/gmail/aaronsmith/mvvmkotlin/User.kt
@@ -0,0 +1,27 @@
+package com.gmail.aaronsmith.mvvmkotlin
+
+//  ┏┓　　　┏┓
+//┏┛┻━━━┛┻┓
+//┃　　　　　　　┃
+//┃　　　━　　　┃
+//┃　┳┛　┗┳　┃
+//┃　　　　　　　┃
+//┃　　　┻　　　┃
+//┃　　　　　　　┃
+//┗━┓　　　┏━┛
+//    ┃　　　┃   神兽保佑
+//    ┃　　　┃   代码无BUG！
+//    ┃　　　┗━━━┓
+//    ┃　　　　　　　┣┓
+//    ┃　　　　　　　┏┛
+//    ┗┓┓┏━┳┓┏┛
+//      ┃┫┫　┃┫┫
+// 
+/**
+ * @author Aaron Smith
+ * @date 2019/3/28.
+ */
+class User() {
+    lateinit var name: String
+    lateinit var password: String
+}
